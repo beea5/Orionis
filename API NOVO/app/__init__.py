1772093e51afdbc07e68e095bfdd67d8675b04bf
beea5 <142271226+beea5@users.noreply.github.com>
2024-11-10 19:53:26 -0300

    Add files via upload

---
+++ API NOVO/app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, template_folder="views", static_folder="../public")
+
+db = SQLAlchemy()
+
+def create_app():
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:127015@localhost:5434/tempo'
+    app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+    app.config["SECRET_KEY"] = 'secret'
+    app.config['JWT_SECRET_KEY'] = 'secret'
+
+    db.init_app(app)
+
+    return app
